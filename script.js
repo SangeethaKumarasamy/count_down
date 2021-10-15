8d037d1528880c28b45b2480fb9fb25e3dffab54
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2021-10-15 08:25:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-let countfrom = 10;
-let timer = document.getElementById("countdown");
+var container=document.createElement("div");
+container.setAttribute("class","container");
+var span=document.getElementById("span")
 
-setInterval( function(){ 
-  countfrom = countfrom - 1
-  if (countfrom <= 0) {
-    timer.innerText = "Happy Independance Day!";
-  } else {
-    timer.innerText = countfrom;
-  }
-}, 1000);
+span.innerHTML=10;
+document.body.append(container);
+let time=10
+setTimeout(()=>{
+    time=time-1
+    span.innerHTML=time
+    setTimeout(()=>{
+        time=time-1
+        span.innerHTML=time
+        setTimeout(()=>{
+            time=time-1
+            span.innerHTML=time
+            setTimeout(()=>{
+                time=time-1
+                span.innerHTML=time
+                setTimeout(()=>{
+                    time=time-1
+                    span.innerHTML=time
+                    setTimeout(()=>{
+                        time=time-1
+                        span.innerHTML=time
+                        setTimeout(()=>{
+                            time=time-1
+                            span.innerHTML=time
+                            setTimeout(()=>{
+                                time=time-1
+                                span.innerHTML=time
+                                setTimeout(()=>{
+                                    time=time-1
+                                    span.innerHTML=time
+                                    setTimeout(()=>{
+                                        time=time-1
+                                        span.innerHTML="Happy Independence Day!!"
+                                },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
 
+            },1000)
+        },1000)
 
+    },1000)
+},1000)
